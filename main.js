176af29bef73a8116e16f5bd748012e0fb19e3c8
MathQnADEV <mathqna.project@gmail.com>
2023-01-31 20:43:19 +0800

    code baru

---
+++ main.js
@@ -0,0 +1,8 @@
+let target = document.getElementById('targetObject');
+
+function ubahLatar(){
+    var input = document.getElementById('inputHexa').value
+    target.style.backgroundColor = input
+    console.log(input)
+}
+
